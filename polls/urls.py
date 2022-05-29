afd89c359f19c18d0ae566db5e76472900cb9ed3
ng3rdstmadgke <ng3rdstmadgke@gmail.com>
2022-05-29 13:32:26 +0900

    enhance polls view

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
 
+# app_nameはテンプレートからURLを参照する場合の名前空間となる
+# 例えば detail は <a href="{% url 'polls:detail' question.id %}">...</a> のように参照できる
+app_name = "polls"
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('<int:question_id>/', views.detail, name="detail"),
+    path('<int:question_id>/results/', views.detail, name="results"),
+    path('<int:question_id>/vote/', views.detail, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django.http import Http404, HttpResponse
 
-# Create your views here.
-from django.http import HttpResponse
+from polls.models import Question
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        "latest_question_list": latest_question_list,
+    }
+    # ショートカットなしのテンプレート描画
+    # template = loader.get_template('polls/index.html')
+    # return HttpResponse(template.render(context, request))
+
+    # ショートカットを利用したテンプレート描画
+    return render(request, "polls/index.html", context)
+
+def detail(request, question_id):
+    # pkが存在しない場合に404を返す
+    # try:
+    #     question = Question.objects.get(pk=question_id)
+    # except Question.DoesNotExist:
+    #     raise Http404("Question does not exists")
+
+    # pkが存在しない場合に404を返す (ショートバージョン)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
+
+def results(request, question_id):
+    response = f"You'relooking at the results of question {question_id}"
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
